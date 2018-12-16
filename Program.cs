b1649ae8cad47a9ab81e217d61c0675bf808bbfe
nayyaung <nayyaung@gmail.com>
2018-12-16 20:24:44 +0800

    Removed assigned values for array

--- Program.cs
+++ Program.cs
@@ -8,27 +8,14 @@ namespace netcore_algo
         static void Main(string[] args)
         {
             var array = new List<int>();
-            
-            array.Add(2);
-            array.Add(5);
-            array.Add(1);
-            array.Add(6);
-            array.Add(3);
-            array.Add(4);
-            var sorted = MergeSort.Sort(array);
-            System.Console.WriteLine("Before");
-            array.ForEach(item => System.Console.Write(item.ToString() + " "));
-            System.Console.WriteLine("\nAfter");
-            sorted.ForEach(item => System.Console.Write(item.ToString() + " "));
-
-            System.Console.WriteLine("\nWith random integers");
+            // instantiate an array with random integer
             array = new List<int>();
             Random rnd = new Random();
             for (int i = 0; i <= 10; i++)
             {
-                array.Add(rnd.Next(1, 9));
+                array.Add(rnd.Next(1, 100));
             }
-            sorted = MergeSort.Sort(array);
+            var sorted = MergeSort.Sort(array);
             System.Console.WriteLine("Before");
             array.ForEach(item => System.Console.Write(item.ToString() + " "));
             System.Console.WriteLine("\nAfter");
