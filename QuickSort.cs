5b91e3013b98924366d6a339bbe0e5bca23800eb
ny <6479987+ny@users.noreply.github.com>
2019-07-15 18:45:05 +0800

    Updated Sort Algo

--- QuickSort.cs
+++ QuickSort.cs
@@ -1,49 +1,42 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Collections;
 
-class QuickSort
-{
+class QuickSort {
 
-    private static int Partition(List<int> unsortedArray, int firstIndex, int lastIndex)
-    {
+    private static int Partition (List<int> unsortedArray, int firstIndex, int lastIndex) {
         var pivot = firstIndex;
         var done = false;
         var i = pivot + 1;
         var j = lastIndex;
         var swap = 0;
-        while (!done)
-        {
-            if (unsortedArray[i] <= unsortedArray[pivot] && i <= j) { i++; }
+        while (!done) {
+            while (i <= j && unsortedArray[i] <= unsortedArray[pivot]) { i++; }
 
-            if (unsortedArray[j] >= unsortedArray[pivot] && i <= j) { j--; }
+            while (i <= j && unsortedArray[j] >= unsortedArray[pivot]) { j--; }
 
-            if (j < i) { done = true; }
-            else
-            {
+            if (j < i) { done = true; } else {
                 swap = unsortedArray[i];
                 unsortedArray[i] = unsortedArray[j];
                 unsortedArray[j] = swap;
             }
         }
-        swap = unsortedArray[pivot];
-        unsortedArray[pivot] = unsortedArray[j];
-        unsortedArray[j] = swap;
-
+        if (pivot != j) {
+            swap = unsortedArray[pivot];
+            unsortedArray[pivot] = unsortedArray[j];
+            unsortedArray[j] = swap;
+        }
         // System.Console.WriteLine("\n[Partition] ");
         // unsortedArray.ForEach(item => System.Console.Write(item.ToString() + " "));
         // System.Console.WriteLine("\nPivot: " + pivot);
         return j;
     }
 
-
-    public static List<int> Sort(List<int> unsortedArray, int firstIndex, int lastIndex)
-    {
-        if (firstIndex < lastIndex)
-        {
-            var pivot = Partition(unsortedArray, firstIndex, lastIndex);
-            QuickSort.Sort(unsortedArray, firstIndex, pivot - 1);
-            QuickSort.Sort(unsortedArray, pivot + 1, unsortedArray.Count - 1);
+    public static List<int> Sort (List<int> unsortedArray, int firstIndex, int lastIndex) {
+        if (firstIndex < lastIndex) {
+            var pivot = Partition (unsortedArray, firstIndex, lastIndex);
+            QuickSort.Sort (unsortedArray, firstIndex, pivot - 1);
+            QuickSort.Sort (unsortedArray, pivot + 1, unsortedArray.Count - 1);
         }
         return unsortedArray;
     }
